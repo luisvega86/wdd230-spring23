e1a8ad7f6d83cd6b11d08368f8e22e5e132a2a1d
luisvega86 <rusonic@gmail.com>
2023-04-29 20:58:56 -0300

    dinamic year footer

--- week02/scripts/scripts.js
+++ week02/scripts/scripts.js
@@ -1,3 +1,6 @@
 const lastUpdate = new Date(document.lastModified);
+document.getElementById('lastUpdate').innerText = lastUpdate;
 
-document.getElementById('lastUpdate').innerText = lastUpdate
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById('year').innerText = year;
