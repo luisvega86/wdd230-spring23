9979154ebf4e83c75b59b6be25a9fa43df13912f
luisvega86 <rusonic@gmail.com>
2023-05-09 02:16:34 -0300

    add datetime

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -6,6 +6,11 @@ function toggleMenu() {
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
+const date = new Date();
+const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+const formattedDate = date.toLocaleDateString('en-US', options);
+document.getElementById('datetime').innerText = formattedDate;
+
 
 const lastModified = new Date(document.lastModified);
 document.getElementById('lastModified').innerText = lastModified
