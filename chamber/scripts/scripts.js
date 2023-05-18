81e9728a984d221e2cf928fc5ffeaf142c045cab
luisvega86 <rusonic@gmail.com>
2023-05-18 09:48:44 -0300

    add heroimg, banner, social media

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -11,6 +11,20 @@ const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric
 const formattedDate = date.toLocaleDateString('en-US', options);
 document.getElementById('datetime').innerText = formattedDate;
 
+const banner = document.getElementById('banner');
+
+function addbanner() {
+    const currentDay = date.getDay();
+    console.log(currentDay);
+    if (currentDay === 1 || currentDay === 3) {
+        banner.classList.add("displays");
+        const p = document.createElement("p");
+        p.innerText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+        banner.appendChild(p);
+    }
+}
+
+addbanner();
 
 const lastModified = new Date(document.lastModified);
 document.getElementById('lastModified').innerText = lastModified
