856c56c7a67bbf2844e58f4f857c4f1302bf0f88
luisvega86 <rusonic@gmail.com>
2023-06-10 22:09:07 -0300

    discover, lazy load, lastvisit

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -32,3 +32,59 @@ document.getElementById('lastModified').innerText = lastModified
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById('year').innerText = year;
+
+const navItems = document.querySelectorAll('#primaryNav li');
+
+navItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        navItems.forEach((navItem) => {
+            navItem.classList.remove('active');
+        });
+    
+    item.classList.add('active');
+    });
+});
+
+// lazyLoading
+
+const images = document.querySelectorAll('[data-src]');
+
+const lazyOptions = {
+    rootMargin: '0px 0px 100px 0px',
+    threshold: 0,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-src');
+
+        img.setAttribute('src', src);
+        img.removeAttribute('data-src');
+        observer.unobserve(img);
+        }
+    });
+}, options);
+
+images.forEach((image) => {
+    observer.observe(image);
+});
+
+// days since last visit
+function getDaysSinceLastVisit() {
+    const lastVisitTimestamp = localStorage.getItem('lastVisitTimestamp');
+    
+    const currentTimestamp = new Date().getTime();
+    
+    if (lastVisitTimestamp) {
+        const timeDifference = currentTimestamp - parseInt(lastVisitTimestamp);
+
+      const daysSinceVisit = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        document.getElementById('daysSinceVisit').textContent = daysSinceVisit;
+    }
+        localStorage.setItem('lastVisitTimestamp', currentTimestamp);
+}
+
+getDaysSinceLastVisit();
